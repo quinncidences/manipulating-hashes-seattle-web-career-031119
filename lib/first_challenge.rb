23f1f47c54e41462e327604ba2571d7680296c7c
Quinn Cox <qrc.inpa@gmail.com>
2019-03-05 13:23:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,15 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == "favorite_icecream_flavors"
+        value.each do
+          delete_if == "strawberry"
+        end
+      end
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
